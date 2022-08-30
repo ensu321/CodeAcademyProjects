9087319f3c9ccfbae91775ed3944931d40b43f0f
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-08-30 22:00:28 +0300

    start of javascript

---
+++ javascript/leason 1 apie javascript ir programavimo pagrindus/index.js
@@ -0,0 +1,13 @@
+
+
+
+function functionGetResult() {
+    
+  const a = parseInt(document.getElementById("valueA").value);
+  const b = parseInt(document.getElementById("valueB").value);
+  const x = a + b;
+
+  console.log(x);
+
+
+}
