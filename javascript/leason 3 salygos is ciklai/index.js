92423a48b6288992d0b8048bcdd364a876e11cb4
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-09-06 21:51:03 +0300

    dfsf

--- javascript/leason 3 salygos is ciklai/index.js
+++ javascript/leason 3 salygos is ciklai/index.js
@@ -1,19 +1,8 @@
-// 
 
+const vardas = "Aleksas"
+const ikiKiekSukti = 0;
 
+for (let i = 10; i>ikiKiekSukti; i--) {
 
-function SuskaiciuotiPlota() {
-    let krastineA = document.getElementById('input1').value;
-let krastineB = document.getElementById('input2').value;
-
-let krastineC = document.getElementById('input3').value;
-let krastineD = document.getElementById('input4').value;
-    let staciakampis1 = krastineA * krastineB;
-    console.log('Pirmoje staciakampio plotis: ', staciakampis1);
-    let staciakampis2 = krastineC * krastineD;
-    console.log('Antrojo staciakampio plotis: ', staciakampis2);
-
-    console.log(staciakampis1 >= staciakampis2 ? "Staciakampis pirmas didesnis" : "Staciakampis pirmas mazesnis");
-    
+    console.log(i, vardas);
 }
-
---
+++ javascript/leason 4 ciklo pletimas ir DOM manipulacija/index.js
@@ -0,0 +1,2 @@
+const a = document.querySelector('.bluetext span');
+a.textContent = "blue"
