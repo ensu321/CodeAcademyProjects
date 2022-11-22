8f0b627481af6ee9a3bb4f51221977b7cf431d57
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-11-22 21:28:41 +0200

    hjh

---
+++ java/leason 1 basics/src/paskaita2/Main.java
@@ -0,0 +1,14 @@
+package paskaita2;
+
+public class Main {
+    public static void main(String[] args) {
+    String tekstas = "Tekstas";
+
+    tekstas.charAt(2);
+    tekstas.length();
+
+
+        System.out.println(tekstas.charAt(2));
+
+    }
+}
