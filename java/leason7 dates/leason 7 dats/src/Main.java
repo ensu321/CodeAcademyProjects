518aa9af6ff86e9afca9d733c6ce05778da3be66
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-11-29 21:58:28 +0200

    fdg
    dfdf

--- java/leason7 dates/leason 7 dats/src/Main.java
+++ java/leason7 dates/leason 7 dats/src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+       LocalDateTime localDateTimeMinus3Hours = localDateTime.minusHours(3);
+        System.out.println(localDateTimeMinus3Hours);
+        LocalDateTime localdateTimePlusDays = localDateTime.plusDays(5);
+        System.out.println(localdateTimePlusDays);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        String localDateTimeFormated = localDateTime.format(format);
+        System.out.println(localDateTimeFormated);
+
+
+
     }
 }
