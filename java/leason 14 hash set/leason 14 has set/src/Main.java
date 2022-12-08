f0a71138607cb42d568ffcacb10a894ad723a175
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-12-08 22:51:45 +0200

    gghf
    gh

--- java/leason 14 hash set/leason 14 has set/src/Main.java
+++ java/leason 14 hash set/leason 14 has set/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashSet<String> stringHashSet = new HashSet<>();
+        stringHashSet.add("nmas");
+        stringHashSet.add("krumas");
+        stringHashSet.add("butelois");
+        stringHashSet.add("kreivas");
+
+        System.out.println(stringHashSet);
+        System.out.println(stringHashSet.size());
+
+
+        System.out.println(stringHashSet.contains("krumas"));
+        if (stringHashSet.contains("krumas")) {
+            stringHashSet.remove("krumas");
+            System.out.println(stringHashSet);
+
+            System.out.println(stringHashSet.size());
+        }
+        stringHashSet.clear();
+        System.out.println(stringHashSet);
+
+
     }
 }
