e05a26eb12834441f14690d1d692e7cfa0f86322
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-12-14 19:45:54 +0200

    end
    ed

--- java/leason 15 paveldejimas/leason 15 inheritance/src/Main.java
+++ java/leason 15 paveldejimas/leason 15 inheritance/src/Main.java
@@ -2,31 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Vaisius vaisius = new Vaisius();
-        vaisius.kasAsEsu();
 
-        EgzotinisVaisius egzotinisVaisius = new EgzotinisVaisius();
-        egzotinisVaisius.kasAsEsu();
 
-        LietuviskasVaisius lietuviskasVaisius = new LietuviskasVaisius();
-        lietuviskasVaisius.kasAsEsu();
+        Person person = new Person("Alekas", "Briliovas");
+        System.out.println(person.toString());
 
-        Ananasas ananasas = new Ananasas();
-        ananasas.kasAsEsu();
 
-        Mangas mangas = new Mangas();
-        mangas.kasAsEsu();
-
-        Kriause kriause = new Kriause();
-        kriause.kasAsEsu();
-
-        Obuolys obuolys = new Obuolys();
-        obuolys.kasAsEsu();
-
-        Alyvinis alyvinis = new Alyvinis();
-        alyvinis.kasAsEsu();
-
-        Antaninis antaninis = new Antaninis();
-        antaninis.kasAsEsu();
     }
 }
