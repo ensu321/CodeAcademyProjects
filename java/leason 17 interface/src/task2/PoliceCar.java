3d56581abb978a22e64f0fd2043403a76dc71315
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-12-15 21:12:24 +0200

    F
    F

--- java/leason 17 interface/src/task2/PoliceCar.java
+++ java/leason 17 interface/src/task2/PoliceCar.java
@@ -1,10 +1,60 @@
 package task2;
+import task1.IsLandVehicle;
 
 
-
-public class PoliceCar implements IsEmergency, IslandVehicle {
+public class PoliceCar implements IsEmergency, IsLandVehicle {
     @Override
     public void soundSiren() {
+
+    }
+
+    @Override
+    public Integer getNumWheels() {
+        return null;
+    }
+
+    @Override
+    public void setNumberWheels() {
+
+    }
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void setName() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public int getMaxPassengers() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxPassengers(int maxPassengers) {
+
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxSpeed() {
 
     }
 }
