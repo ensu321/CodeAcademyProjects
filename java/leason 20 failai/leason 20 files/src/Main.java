1c8ca5df527f67446a025b52a021d6c03f035452
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2022-12-20 22:05:03 +0200

    s
    s

--- java/leason 20 failai/leason 20 files/src/Main.java
+++ java/leason 20 failai/leason 20 files/src/Main.java
@@ -1,5 +1,38 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        String name = "Akejheas SASAS";
+        File file = new File("src/vardas.txt");
+        File outputfile = new File("src/rezultas.txt");
+        writeData(name, file);
+        readAndReverseLines(file, outputfile);
+
     }
+    public static void writeData(String data, File file) throws IOException {
+        if(!file.exists()) {
+            file.createNewFile();
+        }
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write(data);
+        bw.close();
+    }
+
+    public static void readAndReverseLines(File inputFile, File outputFile ) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            bw.write(new StringBuilder(line).reverse().toString());
+        }
+        br.close();
+        bw.close();
+
+    }
+
+
+
 }
